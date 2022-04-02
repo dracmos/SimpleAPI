ae07b3f1b1e9cb0a5ac9a2aa81d6a046bda27c64
Casimiro Greco <casimirogreco@gmail.com>
2022-04-02 19:40:27 +0200

    new commit before autodeploy

--- SimpleAPI/Controllers/ValuesController.cs
+++ SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "benvenuto", "da","Casimiro" };
+            return new string[] { "benvenuto", "da","Casimiro", "Greco" };
         }
 
         [HttpGet("{id}")]
